5ec101077b9117077ad78a9d68e74e61d23e8202
Barış Şahin <baris.sahin@zenobilisim.com>
2024-03-23 12:05:46 +0300

    vercel json

--- src/helpers/sync.js
+++ src/helpers/sync.js
@@ -6,6 +6,8 @@
 
 module.exports = async function() {
 
+    //return null;
+
     /* REMOVE DATABASE */
     const { mongoose } = require('../configs/dbConnection')
     await mongoose.connection.dropDatabase()
